618d2136d48c0a26172472d2bb248b626a85fb1a
PedramAlidust <karsite1234@gmail.com>
2023-12-04 19:46:22 +0330

    update controller

--- app/Http/Controllers/Api/TherapistController.php
+++ app/Http/Controllers/Api/TherapistController.php
@@ -74,7 +74,7 @@ class TherapistController extends Controller
             ->first();
 
         if ($existingTherapist) {
-            return response()->json(['error' => 'قبلا ثبت نام کرده اید, جهت ویرایش اطلاعات وارد بخش ویرایش اطلاعات شوید'], 409);
+            return response()->json(['error' => 'قبلا ثبت نام کرده اید, جهت ویرایش اطلاعات به ایمیل karsite123@gmail.com پیام ارسال کنید'], 409);
         }
 
         $therapist = Therapist::create([
