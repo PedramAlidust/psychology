c45b31c675f93a10b17b2dc70b45541cfa5dfe81
PedramAlidust <karsite1234@gmail.com>
2023-12-04 20:20:59 +0330

    update

--- database/migrations/2023_11_16_080746_create_therapists_table.php
+++ database/migrations/2023_11_16_080746_create_therapists_table.php
@@ -21,7 +21,6 @@ return new class extends Migration
 
             $table->string('education');
             $table->string('phone_number');
-            $table->string('therapist_code');
             $table->string('profile_picture')->nullable();
 
             $table->text('description');
